087d09f8ce27cdcb5ad75789c4f2f72e50ba10e9
stetskivostap@gmail.com <stetskivostap@gmail.com>
2020-01-18 22:31:11 +0200

    addedd entities with context and dowlloaded necessary  packages

---
+++ SmokeyWay/DAL/Entities/Gender.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    public class Gender
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Descriprion { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+
+        public Gender()
+        {
+            this.Users = new List<User>();
+        }
+    }
+}
---
+++ SmokeyWay/DAL/Entities/UserRole.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace DAL.Entities
+{
+    public class UserRole
+    {
+        public int Id { get; set; }
+
+        public int Name { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+
+        public UserRole()
+        {
+            this.Users = new List<User>();
+        }
+    }
+}
