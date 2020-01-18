cc1d81c18e3dd53a57c3a3ad0d5a33d35f5b8ab8
stetskivostap@gmail.com <stetskivostap@gmail.com>
2020-01-18 21:31:00 +0200

    added users, dal and bll

---
+++ SmokeyWay/DAL/Entities/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DAL.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Number { get; set; }
+
+        public bool PhoneNumberConfirmed { get; set; }
+
+        public string Email { get; set; }
+
+        public bool EmailConfirmed { get; set; }
+
+        public DateTime BirthDate { get; set; }
+
+        public int GenderId { get; set; }
+
+        public string CommunicationLanguage { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public int RoleId { get; set; }
+    }
+}
