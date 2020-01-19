aaaba68374d2f751b0414ea344844c6c30f79c83
Pavel Rishko <rishkomail.ua@gmail.com>
2020-01-19 10:01:34 +0200

    added entities: order, table, user

---
+++ SmokeyWay/DAL/Entities/Table.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL.Entities
+{
+    public class Table
+    {
+        public Table()
+        {
+            OnlineTableReservations = new List<OnlineTableReservation>();
+            Orders = new List<Order>();
+        }
+
+        public int Id { get; set; }
+
+        public string Identifier { get; set; }
+
+        public int DepartmentId { get; set; }
+
+        public int SeatingCapacity { get; set; }
+
+        public int ConsoleId { get; set; }
+
+        public ICollection<OnlineTableReservation> OnlineTableReservations { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+    }
+}
