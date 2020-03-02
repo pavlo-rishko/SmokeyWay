f129b3d7a9a67f19c9ea720f8dc74e1712d3a156
stetskivostap <43217586+stetskivostap@users.noreply.github.com>
2020-03-02 15:17:33 +0200

    added UserRoleControler (#14)
    * added UserRoleController
    
    * removed whitespace

---
+++ SmokeyWay/DAL/Migrations/20200302103827_UserRoleString.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DAL.Migrations
+{
+    public partial class UserRoleString : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "UserRole",
+                maxLength: 50,
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldMaxLength: 50);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Name",
+                table: "UserRole",
+                type: "int",
+                maxLength: 50,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 50,
+                oldNullable: true);
+        }
+    }
+}
