62246803589a6f22277f6f5c64e1dcb244ca1a65
Pavlo Rishko <pavlorishko.ua@gmail.com>
2020-03-02 17:20:11 +0200

    added offline table reservation controller

---
+++ SmokeyWay/SmokeyWay/Controllers/TableReservationOfflineController.cs
@@ -0,0 +1,128 @@
+﻿using DAL.Entities;
+using DAL.Repository;
+using DAL.UnitOfWork;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmokeyWay.Controllers
+{
+    [Route("api/offlineTableReservations")]
+    [ApiController]
+    public class OfflineTableReservationController : ControllerBase
+    {
+        private readonly IRepositoryBase<OfflineTableReservation> _offlineTableReservationRepository;
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public OfflineTableReservationController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+            _offlineTableReservationRepository = unitOfWork.GetRepository<OfflineTableReservation>();
+        }
+
+        [HttpGet]
+        public IQueryable GetAll()
+        {
+            return _offlineTableReservationRepository.GetAll();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            if (id == default)
+            {
+                throw new ArgumentException($"{nameof(id)} can't be 0");
+            }
+
+            try
+            {
+                var offlineTableReservation = await _offlineTableReservationRepository.Get(x => x.Id == id);
+                return Ok(offlineTableReservation);
+            }
+            catch (Exception ex)
+            {
+                ex.Data["id"] = id;
+                throw;
+            }
+        }
+
+        [HttpPost("create")]
+        public async Task<IActionResult> CreateAsync([FromBody]OfflineTableReservation offlineTableReservation)
+        {
+            if (offlineTableReservation == null)
+            {
+                throw new ArgumentException($"{nameof(offlineTableReservation)} can't be null");
+            }
+
+            try
+            {
+                _offlineTableReservationRepository.Add(offlineTableReservation);
+                await _unitOfWork.SaveChangesAsync();
+                return Ok(offlineTableReservation);
+            }
+            catch
+            {
+                throw new Exception($"Error while adding offline table reservation nameof{nameof(offlineTableReservation)}");
+            }
+        }
+
+        [HttpPut("update/{id}")]
+        public async Task<IActionResult> UpdateById(int id, [FromBody]OfflineTableReservation offlineTableReservation)
+        {
+            if (id == default)
+            {
+                throw new ArgumentException($"{nameof(id)} cannot be 0");
+            }
+
+            try
+            {
+                OfflineTableReservation currentOfflineTableReservation = await _offlineTableReservationRepository.Get(x => x.Id == id);
+
+                if (currentOfflineTableReservation == null)
+                {
+                    throw new NullReferenceException($"Error while updating offline table rReservation. Offline table reservation with {nameof(id)}={id} not found");
+                }
+
+                currentOfflineTableReservation.TableId = offlineTableReservation.TableId;
+                currentOfflineTableReservation.UserName = offlineTableReservation.UserName;
+                currentOfflineTableReservation.UserPhoneNumber = offlineTableReservation.UserPhoneNumber;
+                currentOfflineTableReservation.EmployeeId = offlineTableReservation.EmployeeId;
+
+                _offlineTableReservationRepository.Update(currentOfflineTableReservation);
+                await _unitOfWork.SaveChangesAsync();
+
+                return Ok(currentOfflineTableReservation);
+            }
+            catch (Exception ex)
+            {
+                ex.Data["id"] = id;
+                throw;
+            }
+        }
+
+        [HttpDelete("remove/{id}")]
+        public async Task<IActionResult> RemoveById(int id)
+        {
+            if (id == default)
+            {
+                throw new ArgumentException($"{nameof(id)} cannot be 0");
+            }
+
+            try
+            {
+                var offlineTableReservation = await _offlineTableReservationRepository.Get(x => x.Id == id);
+                _offlineTableReservationRepository.Remove(offlineTableReservation);
+                await _unitOfWork.SaveChangesAsync();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+
+            return Ok();
+        }
+    }
+}
