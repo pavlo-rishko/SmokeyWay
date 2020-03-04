e0db46c26c253cbc28c127c9c73bd56f2696e0c8
Pavlo Rishko <pavlorishko.ua@gmail.com>
2020-03-04 17:19:08 +0200

    remove unused usings

--- SmokeyWay/DataSeeding/SmokeyWayDbContextSeed.cs
+++ SmokeyWay/DataSeeding/SmokeyWayDbContextSeed.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Linq;
 using DAL.Entities;
-using Microsoft.EntityFrameworkCore.Infrastructure;
-using Microsoft.EntityFrameworkCore.Storage;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace DataSeeding
